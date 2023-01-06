29611c15ab16e7d24c59f2922ef13479bba49380
Sonu <s.gupta@axelor.com>
2023-01-06 17:11:35 +0530

    Added Servlet

--- src/main/java/com/unique/main/Test.java
+++ src/main/java/com/unique/main/Test.java
@@ -12,6 +12,7 @@ public class Test extends HttpServlet {
    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 
